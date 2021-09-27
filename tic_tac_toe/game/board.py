3c26b208e87d887c0cba3c0d2f7c7e37a50da5c7
Nicholas Pekular <npekular@np-macbookpro.local>
2021-09-27 15:56:22 -0600

    * Add tests for board size, board full, and cell setting * Add methods to set cell, get board size, check if the board is full * Changed the name of the Game class to GameActions

--- tic_tac_toe/game/board.py
+++ tic_tac_toe/game/board.py
@@ -1,14 +1,16 @@
 from .cell import Cell
+import random
 
 
 class Board:
-    def __init__(self, board_id=""):
+    def __init__(self, board_size=9, board_id=""):
+        self.board_size = 9
         self.board_id = board_id
         self.board = {}
         self.initalize_board()
 
     def initalize_board(self):
-        for c in range(1, 10, 1):
+        for c in range(1, self.board_size + 1, 1):
             self.board[str(c)] = Cell()
 
     def get_board_string(self):
@@ -24,3 +26,18 @@ class Board:
             if c == 3 or c == 6:
                 board_string += '-+-+-\n'
         return board_string + '\n'
+
+    def get_random_cell_number(self):
+        return random.randint(0, self.board_size)
+
+    def set_cell(self, cell_number, value):
+        self.board[str(cell_number)].set_val(value)
+
+    def get_cell_value(self, cell_number):
+        return self.board[str(cell_number)].value
+
+    def is_board_full(self):
+        for k, v in self.board.items():
+            if not v.is_set():
+                return False
+        return True
--- tic_tac_toe/tests/test_board.py
+++ tic_tac_toe/tests/test_board.py
@@ -9,3 +9,31 @@ class TestBoard:
         board_string = Board().get_board_string()
 
         assert board_string == EXPECTED_GAME_BOARD_STRING
+
+    def test_set_cell(self):
+        set_value = 'X'
+        "Set a cell on the board and verify that the cell is set"
+
+        board = Board()
+        random_cell = board.get_random_cell_number()
+        board.set_cell(random_cell, set_value)
+
+        assert board.get_cell_value(random_cell) == set_value
+    
+    def test_get_board_size(self):
+        specified_board_size = 9
+        "Verify that the size of the board is properly returned"
+        
+        board = Board(board_size=specified_board_size)
+        
+        assert board.board_size == specified_board_size
+
+    def test_check_board_is_full(self):
+        "Fill the board and verify that the board shows as full"
+
+        board = Board()
+        for i in range(1, board.board_size + 1, 1):
+            board.set_cell(i, 'X')
+
+        assert board.is_board_full()
+
