2bd5de34f9c84503cedb6eb8e49cb4758946540e
arduinix <arduinix88@gmail.com>
2021-11-05 15:16:01 -0600

    add a BasicAiPlayer class

--- tic_tac_toe/game/__init__.py
+++ tic_tac_toe/game/__init__.py
@@ -2,3 +2,5 @@ from .board import Board
 from .game_actions import GameActions
 from .console_game import ConsoleGame
 from .win_detection import WinDetector
+from .basic_ai_player import BasicAiPlayer
+from .mark import Mark
